aae80363197b5d30547d4011e42df1a58cc531a6
Muhammad ibrahim <Muhammadibrahimtanoli@outlook.com>
2021-04-13 16:34:14 +0500

    adding Movies to the DOM

--- 12-Movie-App/index.js
+++ 12-Movie-App/index.js
@@ -2,14 +2,46 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=c9cca2e412d7378fbfa8c866bf89cd34&query="'
 const form =document.getElementById('form')
-
+const main =document.getElementById('main')
 getMovies(API_URL)
 
-async function getMovies(url) {
-const res = await fetch(url)
-const data = await res.json()
-console.log(data.results)
+ async function getMovies(url) {
+ const res = await fetch(url)
+ const data = await res.json()
+
+ showMovies(data.results)
 }
+function showMovies (movies){
+   main.innerHTML =''
+
+   movies.forEach((movie) => {
+     const {title, poster_path, vote_average, overview} = movie 
+     const movieEl= document.createElement
+     ('div')
+     movieEl.classList.add('movie')
+
+    movieEl.innerHTML=`
+            <img src="${ IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+            <h3> ${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+            <h3>Overview</h3>
+            ${overview}
+            </div>`
+        main.appendChild(movieEl)
+   });
+}
+   function getClassByRate(vote){
+   if (vote >= 8) {
+    return 'green'
+   } else if (vote >=5){
+    return 'orange'
+   } else{
+    return 'red'
+   }
+   }
 
 form.addEventListener('submit', (e)=> {
 e.preventDefault()
