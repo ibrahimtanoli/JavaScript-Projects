51baa5a05cf9e0c19caba3224120a4ca91276a35
Muhammad ibrahim <Muhammadibrahimtanoli@outlook.com>
2021-07-15 16:49:49 +0500

    update index.js
    Delete Note
    Edit Note

--- 26-Notes-App/index.js
+++ 26-Notes-App/index.js
@@ -10,13 +10,33 @@ function addNewNote(text = '') {
         <button class="edit">
             <i class="fas fa-edit"></i>
         </button>
+
         <button class="delete">
             <i class="fas fa-trash-alt"></i>
         </button>
+
     </div>
+
     <div class="main ${text ? '' : 'hidden'}"></div>
     <textarea class="${text ? 'hidden' : ''}"></textarea>
 `
+  const editBtn = note.querySelector('.edit')
+  const deleteBtn = note.querySelector('.delete')
+  const main = note.querySelector('.main')
+  const textArea = note.querySelector('textarea')
+
+  // Delete Note
+
+  deleteBtn.addEventListener('click', () => {
+    note.remove()
+  })
+
+  // Edit Note
+
+  editBtn.addEventListener('click', () => {
+    main.classList.toggle('hidden')
+    textArea.classList.toggle('hidden')
+  })
 
   document.body.appendChild(note)
 }
