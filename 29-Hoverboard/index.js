9a03b1b55d30664433b355fb40f0a4bb5fba5cce
Muhammad ibrahim <Muhammadibrahimtanoli@outlook.com>
2021-07-31 15:07:44 +0500

    update index.js

---
+++ 29-Hoverboard/index.js
@@ -0,0 +1,33 @@
+const container = document.getElementById('container')
+
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+
+let SQUARES = 600
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+
+  square.addEventListener('mouseover', () => setColor(square))
+
+  square.addEventListener('mouseout', () => removeColor(square))
+
+  container.appendChild(square)
+}
+
+//      Set-color
+function setColor(element) {
+  const color = getRandomColor()
+  element.style.background = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+//      Remove-color
+function removeColor(element) {
+  element.style.background = '#1d1d1d'
+  element.style.boxShadow = '0 0 2px #000'
+}
+//       Random-Color
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
